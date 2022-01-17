da6ff0321a5967d4dc06ca1241708dc54535a5e4
alexandrova-s <37410394+alexandrova-s@users.noreply.github.com>
2022-01-17 23:47:56 +0000

    Update replacements.py

--- replacements.py
+++ replacements.py
@@ -1,6 +1,6 @@
 from similarities import find_similarities, get_indexes_of_minima 
 
-def replace_vocals(voc1, voc2, full_vocal1, full_vocal2, number_of_fragments_to_replace):
+def replace_vocals(voc1, voc2, full_vocal1, full_vocal2, accompaniament):
     list_of_minima = []
     all_similarities = list(find_similarities(voc1, voc2))
     for x in all_similarities:
@@ -10,19 +10,25 @@ def replace_vocals(voc1, voc2, full_vocal1, full_vocal2, number_of_fragments_to_
     sorted_indexes = get_indexes_of_minima(list_of_minima)
     all_start_moments = []
     all_stop_moments = []
-    for index_of_minimum in range(0, len(list_of_minima)):  #sorted_indexes[:number_of_fragments_to_replace]
+    # for index_of_minimum in range(0, len(list_of_minima)):  #sorted_indexes[:number_of_fragments_to_replace]
+    #     to_replace = all_similarities[index_of_minimum]
+    #     all_start_moments.append(to_replace[1][0].stop)
+    #     all_stop_moments.append(to_replace[1][0].start)
+    # all_start_moments = sorted(all_start_moments)
+    # all_stop_moments = sorted(all_stop_moments)
+    # for i in range(0, len(all_start_moments)):
+    #     # print(all_start_moments[i])
+    #     # print(all_stop_moments[i])
+    #     print(f"Podmiana w: {to_replace[0].start}")
+    #     print(to_replace[1][0].stop)
+    #     vocal_replacement = full_vocal2[to_replace[1][0].stop:to_replace[1][0].start]
+    for index_of_minimum in sorted_indexes:
         to_replace = all_similarities[index_of_minimum]
-        all_start_moments.append(to_replace[1][0].stop)
-        all_stop_moments.append(to_replace[1][0].start)
-    all_start_moments = sorted(all_start_moments)
-    all_stop_moments = sorted(all_stop_moments)
-    for i in range(0, len(all_start_moments)):
-        print(all_start_moments[i])
-        print(all_stop_moments[i])
-        # print(f"Podmiana w: {to_replace[0].start}")
-        # print(to_replace[1][0].stop)
-        # vocal_replacement = full_vocal2[to_replace[1][0].stop:to_replace[1][0].start]
-    # return vocal_replacement
+        print(to_replace)
+        print(f"Podmiana w: {to_replace[0].start}")
+        vocal_replacement = full_vocal2[to_replace[1][0].stop:to_replace[1][0].start]
+        accompaniament = accompaniament.overlay(vocal_replacement, position=to_replace[0].start)
+    return accompaniament
 
 def replace_vocals_accompaniament(voc1, voc2, full_vocal1, full_vocal2, accompaniament): #, number_of_fragments_to_replace):
     list_of_minima = []
